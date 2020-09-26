d8b266e9cee5fa3c04af26eb8b94897505805c1e
Omar El Halawany <omar.el-halawany@hotmail.com>
2020-09-26 11:27:42 +0200

    fixed handle submit on adding new todo so that I dont use document.get to get input values, used createRef instead

--- src/components/addTodo.jsx
+++ src/components/addTodo.jsx
@@ -2,40 +2,27 @@ import React, { Component } from "react";
 import "../css/AddTodo.css";
 
 class AddTodo extends Component {
-  /*state = {
-    newTodo: "",
-  }; (first solution)*/
+  newTodo = React.createRef();
+
   handleSubmit = (e) => {
     e.preventDefault();
-    // this.props.handleAddTodo(this.state.newTodo); (first solution)
-    //second solution better cuz its easy to reset input>>
-
-    let inpt = document.getElementById("newTodo");
-    if (inpt.value === null || inpt.value.match(/^ *$/) !== null) {
+    let newTodo = this.newTodo.current.value;
+    if (newTodo === null || newTodo.match(/^ *$/) !== null) {
       return;
     }
-    let newTodo = inpt.value;
-    inpt.value = "";
     this.props.handleAddTodo(newTodo);
+    newTodo = "";
   };
-
-  /*
-  first solution
-  handleNewTodo = (e) => {
-    let newTodo = { ...this.state.newTodo };
-    newTodo = e.target.value;
-    this.setState({ newTodo });
-  };*/
 
   render() {
     return (
       <React.Fragment>
         <form className="form" onSubmit={this.handleSubmit}>
           <input
+            ref={this.newTodo}
             type="text"
             id="newTodo"
             placeholder="Add new todo . . ."
-            /*onChange={this.handleNewTodo} (first solution)*/
           />
           <button type="submit" className="btn btn-primary btn-sm">
             Add todo
